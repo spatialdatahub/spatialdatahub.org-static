a2b798b83de3696dff7769521029a2089bcece27
patcurry <currypat1985@gmail.com>
2018-03-22 11:07:32 +0100

    finished the editMap.js tests

--- src/__tests__/editMap.test.js
+++ src/__tests__/editMap.test.js
@@ -1,7 +1,8 @@
 // test/editMap.test.js
 'use strict';
 
-import { showPlaceContainer } from '../pieces/editMap.js';
+import { showPlaceContainer, getSelectedPlacePolygon } from '../pieces/editMap.js';
+import L from 'leaflet';
 
 describe('editMap.js file tests', () => {
     // Is this an extraneous function?
@@ -22,6 +23,7 @@ describe('editMap.js file tests', () => {
             // test to see that the function worked
             expect(element.getAttribute('style')).toBe('display: block;');
         });
+
         test('showPlaceContainer should change element display style from "none" to "block"', () => {
             // mock the document
             const fixture = document.createElement('div');
@@ -38,5 +40,103 @@ describe('editMap.js file tests', () => {
             // test to see that the function worked
             expect(element.getAttribute('style')).toBe('display: block;');
         });
+    });
+
+    describe('getSelectedPlacePolygon', () => {
+        // I should be importing these from their own files. 
+        // triangle polygon
+        const poly = {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "properties": {},
+                    "geometry": {
+                        "type": "Polygon",
+                        "coordinates": [
+                            [
+                                [
+                                    -18.544921875,
+                                    29.53522956294847
+                                ],
+                                [
+                                    -8.9208984375,
+                                    22.30942584120019
+                                ],
+                                [
+                                    -9.31640625,
+                                    31.39115752282472
+                                ],
+                                [
+                                    -18.544921875,
+                                    29.53522956294847
+                                ]
+                            ]
+                        ]
+                    }
+                }
+            ]
+        };
+
+        const line = {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "properties": {},
+                    "geometry": {
+                        "type": "LineString",
+                        "coordinates": [
+                            [
+                                -13.9306640625,
+                                33.87041555094183
+                            ],
+                            [
+                                2.4609375,
+                                27.72243591897343
+                            ]
+                        ]
+                    }
+                }
+            ]
+        };
+
+        const point = {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "properties": {},
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [
+                            -4.482421875,
+                            38.65119833229951
+                        ]
+                    }
+                }
+            ]
+        };
+
+        const polyLayer = L.geoJson(poly);
+        const lineLayer = L.geoJson(line);
+        const pointLayer = L.geoJson(point);
+
+        test('getSelectedPlacePolygon should return "not a polygon" if given a point layer', () => {
+            const arr = [pointLayer];
+            expect(getSelectedPlacePolygon(arr)).toBe('not a polygon');
+        });
+
+        test('getSelectedPlacePolygon should return "not a polygon" if given a line layer', () => {
+            const arr = [lineLayer];
+            expect(getSelectedPlacePolygon(arr)).toBe('not a polygon');
+        });
+
+        // maybe not
+        // it returns a featureCollection
+        //      test('getSelectedPlacePolygon should return "Polygon" if given a polygon layer', () => {
+        //          const arr = [polyLayer];
+        //          expect(getSelectedPlacePolygon(arr)).toBe('not a polygon');
+        //      });
     });
 });
